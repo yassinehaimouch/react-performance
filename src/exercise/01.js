00f1d36176f5a726b4081cc237909d715c4d7976
yassinehaimouch <haymouch53@gmail.com>
2023-05-11 15:21:46 +0100

    Exercice 1 Solution with Magic comment

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,8 +3,7 @@
 
 import * as React from 'react'
 
-const loadGlob = () => import('../globe')
-const Globe = React.lazy(loadGlob)
+const Globe = React.lazy(() => import(/* webpackPrefetch: true */ '../globe'))
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
@@ -20,11 +19,7 @@ function App() {
         padding: '2rem',
       }}
     >
-      <label
-        style={{marginBottom: '1rem'}}
-        onMouseEnter={loadGlob}
-        onFocus={loadGlob}
-      >
+      <label style={{marginBottom: '1rem'}}>
         <input
           type="checkbox"
           checked={showGlobe}
